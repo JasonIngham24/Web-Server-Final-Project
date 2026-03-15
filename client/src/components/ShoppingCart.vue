57e3e982a02ac9e9e3ec34bd765fcf309a9cede6
inghamj1 <inghamj1@newpaltz.edu>
2026-03-15 10:33:50 -0400

    Implemented Shopping cart

--- client/src/components/ShoppingCart.vue
+++ client/src/components/ShoppingCart.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
+import { useCartStore } from '@/stores/cart';
 
+const cart = useCartStore();
 </script>
 
 <template>
   <div>
     <h1 class="title is-1">Shopping Cart</h1>
-    <p>Your cart is currently empty.</p>
+    <p v-if="cart.items.length === 0">Your cart is empty.</p>
+    <ul v-else>
+      <li v-for="item in cart.items" :key="item.product.id" class = "cart-item">
+        <img :src="item.product.thumbnail" alt="Product Image" class="image is-64x64">
+        {{ item.product.title }} - ${{ item.product.price }} x {{ item.quantity }}
+      </li>
+    </ul>
   </div>
 </template>
 
